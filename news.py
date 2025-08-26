5956600d4d91b299ca58f0c92561de66a1e3fcb3
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-08-26 12:22:17 +0530

    Update news.py

--- news.py
+++ news.py
@@ -4,7 +4,7 @@ import streamlit as st
 import requests
 
 API_KEY = "3dd9352c1f42423388bb8e4e1a337e44"
-query = "recent stock market news"
+query = "NSE stock market news"
 
 url = f"https://newsapi.org/v2/everything?q={query}&apiKey={API_KEY}&pageSize=30"
 
@@ -18,4 +18,5 @@ for article in response.get("articles", []):
         st.image(article["urlToImage"], width=200)
     st.write(article.get("description", ""))
     st.markdown("---")
+
 
