f8ac2120278c05536f2fd108f6e6bd3fd44262e2
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-08-26 12:18:25 +0530

    Update and rename page2.py to news.py

--- news.py
+++ news.py
@@ -4,9 +4,9 @@ import streamlit as st
 import requests
 
 API_KEY = "3dd9352c1f42423388bb8e4e1a337e44"
-query = "stock market"
+query = "recent stock market news"
 
-url = f"https://newsapi.org/v2/everything?q={query}&apiKey={API_KEY}&pageSize=5"
+url = f"https://newsapi.org/v2/everything?q={query}&apiKey={API_KEY}&pageSize=30"
 
 response = requests.get(url).json()
 
@@ -18,3 +18,4 @@ for article in response.get("articles", []):
         st.image(article["urlToImage"], width=200)
     st.write(article.get("description", ""))
     st.markdown("---")
+
