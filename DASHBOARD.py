c41c647890b0cb2da1da3113bc2f3ae3586af0e6
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-08-26 10:54:39 +0530

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+#Page Setup#
+about_page=st.Page(
+  page="stock_analysis.py",
+  title="MAIN PAGE",
+  default=True,
+)
+
+pg= st.navigation(pages=[about_page])
+
+pg.run
+
 st.title("🏠 Home Page")
 st.write("Welcome to the Stock Diary App!")
+
